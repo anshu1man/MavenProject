584e2adad58fb7ee433135905801612e54173d32
anshu1man <anshus_singh_17@yahoo.co.in>
2021-12-12 15:43:30 +0530

    commit done

--- src/test/java/StepDefinitions/GoogleSearchStep.java
+++ src/test/java/StepDefinitions/GoogleSearchStep.java
@@ -2,26 +2,50 @@ package StepDefinitions;
 
 import java.util.concurrent.TimeUnit;
 
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
+import io.cucumber.java.Before;
+import io.cucumber.java.BeforeStep;
 import io.cucumber.java.en.*;
 import junit.framework.Assert;
 
 public class GoogleSearchStep {
 
 	WebDriver driver = null;
+	
+	@Before
+	public void Browser_Initialization_Block() {
+		
+		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/Drivers/chromedriver.exe");
+		driver = new ChromeDriver();  
+		driver.manage().window().maximize();
+		System.out.println("Browser is initialised");
+		//driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+		
+		
+	}
+	
+	@BeforeStep
+	public void BefoteStep() {
+		
+		System.out.println("This is before step");
+	
+		
+		
+	}
 
 	
 	@Given("Browser is open")
 	public void browser_is_open() {
 		
-		System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"/Drivers/chromedriver.exe");
-		driver = new ChromeDriver();  
-		driver.manage().window().maximize();
-		//driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+		
+		driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
 		
 		
 	}
@@ -48,12 +72,29 @@ public class GoogleSearchStep {
 
 	@Then("^User Successfully navigate to(.*)$")
 	public void user_Successfully_navigate_to(String strtext) {
-		//String tigerSearchText = driver.findElement(By.xpath("//*[@id='rso']//a/h3[contains(text(),'"+strtext+"')]")).getText();
-		//if(tigerSearchText.contains(strtext))
+		String tigerSearchText = driver.findElement(By.xpath("//*[@id='rso']//a/h3[contains(text(),'"+strtext+"')]")).getText();
+		if(tigerSearchText.contains(strtext))
 			Assert.assertTrue(true);
-	//	else
-		//	Assert.assertTrue(false);
+		else
+			Assert.assertTrue(false);
+		
+		
+	}
+	
+	@After
+	public void Browser_Quit_Block() {
+		
 		driver.quit();
+		System.out.println("Browser is closed");
+		
+		
+	}
+	@AfterStep
+	public void Afterstep() {
+		
+		
+		System.out.println("This is after step");
+		
 		
 	}
 
--- target/test-classes/Features/GoogleSearch.feature
+++ target/test-classes/Features/GoogleSearch.feature
@@ -1,5 +1,19 @@
 Feature: Check The Search Functionality at Google Search Engine
-@smoke
+
+  Scenario Outline: Check if User is able Search Successfully at Google
+    Given Browser is open
+    And Open a Google Search Engine
+    When User enters the <Text> to be Searched
+    And User clicks on Search button
+    Then User Successfully navigate to<Text>
+
+    Examples: 
+      | Text     |
+      | Tiger    |
+      | Lion     |
+      | Elephant |
+      
+     @smoke 
   Scenario Outline: Check if User is able Search Successfully at Google
     Given Browser is open
     And Open a Google Search Engine
